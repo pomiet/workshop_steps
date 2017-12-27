7be0ee8d2877d1be985f451b00edf6da59f38357
Rob Keefer <rob.keefer@pomiet.com>
2017-12-27 11:43:52 -0500

    Setup 10 Genservers and test them.

--- lib/table_supervisor_five.ex
+++ lib/table_supervisor_five.ex
@@ -10,7 +10,14 @@ defmodule TableSupervisorFive do
     children = [
       worker(TableServerFive, [0, "021M"], [id: "021M"]),
       worker(TableServerFive, [1000000, "122M"], [id: "122M"]),
-      worker(TableServerFive, [2000000, "223M"], [id: "223M"])
+      worker(TableServerFive, [2000000, "223M"], [id: "223M"]),
+      worker(TableServerFive, [3000000, "324M"], [id: "324M"]),
+      worker(TableServerFive, [4000000, "425M"], [id: "425M"]),
+      worker(TableServerFive, [5000000, "526M"], [id: "526M"]),
+      worker(TableServerFive, [6000000, "627M"], [id: "627M"]),
+      worker(TableServerFive, [7000000, "728M"], [id: "728M"]),
+      worker(TableServerFive, [8000000, "829M"], [id: "829M"]),
+      worker(TableServerFive, [9000000, "921B"], [id: "921B"])
     ]
     opts = [strategy: :one_for_one, name: TableServerFive.Supervisor]
 
--- test/table_supervisor_five_test.exs
+++ test/table_supervisor_five_test.exs
@@ -12,17 +12,33 @@ defmodule TableSupervisorFiveTest do
   test "supervisor restarts after exit", %{server: server_pid} do
     assert {:ok, 0} == TableServerFive.ping("021M")
     assert {:ok, 1} == TableServerFive.pong("021M")
-    assert {:ok, 2} == TableServerFive.ping("021M")
-    assert {:ok, 3} == TableServerFive.pong("021M")
 
     assert {:ok, 1000000} == TableServerFive.ping("122M")
     assert {:ok, 1000001} == TableServerFive.pong("122M")
-    assert {:ok, 1000002} == TableServerFive.ping("122M")
-    assert {:ok, 1000003} == TableServerFive.pong("122M")
 
     assert {:ok, 2000000} == TableServerFive.ping("223M")
     assert {:ok, 2000001} == TableServerFive.pong("223M")
-    assert {:ok, 2000002} == TableServerFive.ping("223M")
-    assert {:ok, 2000003} == TableServerFive.pong("223M")
+
+    assert {:ok, 3000000} == TableServerFive.ping("324M")
+    assert {:ok, 3000001} == TableServerFive.pong("324M")
+
+    assert {:ok, 4000000} == TableServerFive.ping("425M")
+    assert {:ok, 4000001} == TableServerFive.pong("425M")
+
+    assert {:ok, 5000000} == TableServerFive.ping("526M")
+    assert {:ok, 5000001} == TableServerFive.pong("526M")
+
+    assert {:ok, 6000000} == TableServerFive.ping("627M")
+    assert {:ok, 6000001} == TableServerFive.pong("627M")
+
+    assert {:ok, 7000000} == TableServerFive.ping("728M")
+    assert {:ok, 7000001} == TableServerFive.pong("728M")
+
+    assert {:ok, 8000000} == TableServerFive.ping("829M")
+    assert {:ok, 8000001} == TableServerFive.pong("829M")
+
+    assert {:ok, 9000000} == TableServerFive.ping("921B")
+    assert {:ok, 9000001} == TableServerFive.pong("921B")
+
   end
 end
