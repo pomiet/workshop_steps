dc8d9ea8d5ed09266d94dcd80fb47e142cfcbc11
Rob Keefer <rob.keefer@pomiet.com>
2017-12-28 13:00:15 -0500

    Update process tests

--- lib/table_server_zero.ex
+++ lib/table_server_zero.ex
@@ -8,7 +8,19 @@ defmodule TableServerZero do
     ping
   end
 
+  def pong do
+    receive do
+      {from_pid, :pong} ->
+        IO.puts 'received pong, send ping'
+        :timer.sleep(1000)
+        send from_pid, {self(), :ping}
+    end
+    pong
+  end
+
   def start do
-    spawn(__MODULE__, :ping, [])
+    ping_pid = spawn(__MODULE__, :ping, [])
+    pong_pid = spawn(__MODULE__, :pong, [])
+    {ping_pid, pong_pid}
   end
 end
--- test/table_server_zero_test.exs
+++ test/table_server_zero_test.exs
@@ -2,10 +2,14 @@ defmodule TableServerZeroTest do
   use ExUnit.Case
 
   test "receive pong from ping" do
-    to_pid = TableServerZero.start()
-    send(to_pid, {self(), :ping})
+    {ping_pid, pong_pid} = TableServerZero.start()
 
-    assert_receive({to_spid, :pong})
-    refute to_pid == self()
+    send(ping_pid, {self(), :ping})
+    assert_receive({ping_spid, :pong}, 1500)
+    refute ping_pid == self()
+
+    send(pong_pid, {self(), :pong})
+    assert_receive({pong_spid, :ping}, 1500)
+    refute pong_pid == self()
   end
 end
